83777a31b2f98186e16c1d9d62a350cec3b2dd13
Luiz Castro <fellipeluizz@gmail.com>
2018-03-14 10:58:45 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+const config = {
+  entry: './src/main.js',
+  output: {
+    filename: './build.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        ecma: 8,
+        warnings: false,
+        compress: {warnings: false},
+        output: {
+          comments: false,
+          beautify: false
+        },
+        toplevel: false,
+        nameCache: null,
+        ie8: false,
+        keep_classnames: undefined,
+        keep_fnames: false
+      }
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+}
+
+module.exports = config
